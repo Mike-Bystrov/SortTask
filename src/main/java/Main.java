e7a5197caa952fa8cd8ab29d4a449d6d60081e53
Mike B <fpm.bystrov@bsu.by>
2024-02-10 21:36:38 +0300

    small fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
 import repository.FileProcessorImpl;
-import service.SortService;
 import service.SortServiceImpl;
 
 import java.util.ArrayList;
@@ -8,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         FileProcessorImpl fileProcessor = new FileProcessorImpl();
         SortServiceImpl service = new SortServiceImpl();
+
         ArrayList<String[]> arr = fileProcessor.readArrayFromFile("in.txt");
         service.sortArray(arr);
         fileProcessor.writeInfo(arr, "out.txt");
